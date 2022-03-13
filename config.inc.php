0fec7824533470c5f9bfa78518e00a80911c11ef
omarbahaa20 <omarbahaamyportfolio@gmail.com>
2022-03-13 14:37:19 +0200

    223

--- config.inc.php
+++ config.inc.php
@@ -1,169 +1,68 @@
 <?php
-/**
- * phpMyAdmin sample configuration, you can use it as base for
- * manual configuration. For easier setup you can use setup/
- *
- * All directives are explained in documentation in the doc/ folder
- * or at <https://docs.phpmyadmin.net/>.
- */
 
-declare(strict_types=1);
-
-/**
- * This is needed for cookie based authentication to encrypt password in
- * cookie. Needs to be 32 chars long.
- */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
-
-/**
- * Servers configuration
- */
+/* Servers configuration */
 $i = 0;
+/* The 'cookie' auth_type uses AES algorithm to encrypt the password. If
+ * at least one server configuration uses 'cookie' auth_type, enter here a
+ * pass phrase that will be used by AES. The minimum length is 32 characters
+ * The maximum length seems to be 46 characters. */
+$cfg['blowfish_secret'] = 'h]C+{nqW$omNoTIkCwC$%z-LTcy%p6_j';
 
-/**
- * First server
- */
+//Checking Active DBMS Servers
+$wampConf = @parse_ini_file('../../wampmanager.conf');
+//Check if MySQL and MariaDB with MariaDB on default port
+$mariaFirst = ($wampConf['SupportMySQL'] == 'on' && $wampConf['SupportMariaDB'] == 'on' && $wampConf['mariaPortUsed'] == $wampConf['mysqlDefaultPort']) ? true : false;
+if($wampConf['SupportMySQL'] == 'on') {
+/* Server: localhost [1] */
+	$i++;
+	if($mariaFirst) $i++;
+	$cfg['Servers'][$i]['verbose'] = 'MySQL';
+	$cfg['Servers'][$i]['host'] = '127.0.0.1';
+	$cfg['Servers'][$i]['port'] = $wampConf['mysqlPortUsed'];
+	$cfg['Servers'][$i]['extension'] = 'mysqli';
+	$cfg['Servers'][$i]['auth_type'] = 'cookie';
+	$cfg['Servers'][$i]['user'] = '';
+	$cfg['Servers'][$i]['password'] = '';
+
+	// Hidden databases in PhpMyAdmin left panel
+	//$cfg['Servers'][$i]['hide_db'] = '(information_schema|mysql|performance_schema|sys)';
+
+	// Allow connection without password
+	$cfg['Servers'][$i]['AllowNoPassword'] = true;
+}
+/* Server: localhost [2] */
 $i++;
-/* Authentication type */
-$cfg['Servers'][$i]['auth_type'] = 'cookie';
-/* Server parameters */
-$cfg['Servers'][$i]['host'] = 'localhost';
-$cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = false;
-
-/**
- * phpMyAdmin configuration storage settings.
- */
-
-/* User used to manipulate with storage */
-// $cfg['Servers'][$i]['controlhost'] = '';
-// $cfg['Servers'][$i]['controlport'] = '';
-// $cfg['Servers'][$i]['controluser'] = 'pma';
-// $cfg['Servers'][$i]['controlpass'] = 'pmapass';
-
-/* Storage database and tables */
-// $cfg['Servers'][$i]['pmadb'] = 'phpmyadmin';
-// $cfg['Servers'][$i]['bookmarktable'] = 'pma__bookmark';
-// $cfg['Servers'][$i]['relation'] = 'pma__relation';
-// $cfg['Servers'][$i]['table_info'] = 'pma__table_info';
-// $cfg['Servers'][$i]['table_coords'] = 'pma__table_coords';
-// $cfg['Servers'][$i]['pdf_pages'] = 'pma__pdf_pages';
-// $cfg['Servers'][$i]['column_info'] = 'pma__column_info';
-// $cfg['Servers'][$i]['history'] = 'pma__history';
-// $cfg['Servers'][$i]['table_uiprefs'] = 'pma__table_uiprefs';
-// $cfg['Servers'][$i]['tracking'] = 'pma__tracking';
-// $cfg['Servers'][$i]['userconfig'] = 'pma__userconfig';
-// $cfg['Servers'][$i]['recent'] = 'pma__recent';
-// $cfg['Servers'][$i]['favorite'] = 'pma__favorite';
-// $cfg['Servers'][$i]['users'] = 'pma__users';
-// $cfg['Servers'][$i]['usergroups'] = 'pma__usergroups';
-// $cfg['Servers'][$i]['navigationhiding'] = 'pma__navigationhiding';
-// $cfg['Servers'][$i]['savedsearches'] = 'pma__savedsearches';
-// $cfg['Servers'][$i]['central_columns'] = 'pma__central_columns';
-// $cfg['Servers'][$i]['designer_settings'] = 'pma__designer_settings';
-// $cfg['Servers'][$i]['export_templates'] = 'pma__export_templates';
-
-/* Heroku remote server */
-$i++;
-$cfg['Servers'][$i]['verbose'] = 'ClearDB';
-$cfg["Servers"][$i]["host"] = "mysql.colorvision.design"; //provide hostname
+if($mariaFirst) $i -= 2;
+$cfg['Servers'][$i]['verbose'] = 'MariaDB';
+$cfg['Servers'][$i]['host'] = 'mysql.colorvision.design';
 $cfg['Servers'][$i]['extension'] = 'mysqli';
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['user'] = '';
 $cfg['Servers'][$i]['password'] = '';
 
-/**
- * End of servers configuration
- */
+// Hidden databases in PhpMyAdmin left panel
+//$cfg['Servers'][$i]['hide_db'] = '(information_schema|mysql|performance_schema|sys)';
+// Allow connection without password
+$cfg['Servers'][$i]['AllowNoPassword'] = true;
 
-/**
- * Directories for saving/loading files from server
- */
-$cfg['UploadDir'] = '';
-$cfg['SaveDir'] = '';
+/* Heroku remote server */
+$i++;
+$cfg['Servers'][$i]['verbose'] = 'ClearDB';
+$cfg["Servers"][$i]["host"] = "eu-cdbr-west-02.cleardb.net"; //provide hostname
+$cfg["Servers"][$i]["user"] = "b214e938e157d1"; //user name for your remote server
+$cfg["Servers"][$i]["password"] = "854c8f16"; //password
+$cfg["Servers"][$i]["auth_type"] = "config"; // keep it as config
 
-/**
- * Whether to display icons or text or both icons and text in table row
- * action segment. Value can be either of 'icons', 'text' or 'both'.
- * default = 'both'
- */
-//$cfg['RowActionType'] = 'icons';
 
-/**
- * Defines whether a user should be displayed a "show all (records)"
- * button in browse mode or not.
- * default = false
- */
-//$cfg['ShowAll'] = true;
 
-/**
- * Number of rows displayed when browsing a result set. If the result
- * set contains more rows, "Previous" and "Next".
- * Possible values: 25, 50, 100, 250, 500
- * default = 25
- */
-//$cfg['MaxRows'] = 50;
+// Suppress Warning about pmadb tables
+$cfg['PmaNoRelation_DisableWarning'] = true;
 
-/**
- * Disallow editing of binary fields
- * valid values are:
- *   false    allow editing
- *   'blob'   allow editing except for BLOB fields
- *   'noblob' disallow editing except for BLOB fields
- *   'all'    disallow editing
- * default = 'blob'
- */
-//$cfg['ProtectBinary'] = false;
+// To have PRIMARY & INDEX in table structure export
+$cfg['Export']['sql_drop_table'] = true;
+$cfg['Export']['sql_if_not_exists'] = true;
 
-/**
- * Default language to use, if not browser-defined or user-defined
- * (you find all languages in the locale folder)
- * uncomment the desired line:
- * default = 'en'
- */
-//$cfg['DefaultLang'] = 'en';
-//$cfg['DefaultLang'] = 'de';
+$cfg['MySQLManualBase'] = 'http://dev.mysql.com/doc/refman/5.7/en/';
+/* End of servers configuration */
 
-/**
- * How many columns should be used for table display of a database?
- * (a value larger than 1 results in some information being hidden)
- * default = 1
- */
-//$cfg['PropertiesNumColumns'] = 2;
-
-/**
- * Set to true if you want DB-based query history.If false, this utilizes
- * JS-routines to display query history (lost by window close)
- *
- * This requires configuration storage enabled, see above.
- * default = false
- */
-//$cfg['QueryHistoryDB'] = true;
-
-/**
- * When using DB-based query history, how many entries should be kept?
- * default = 25
- */
-//$cfg['QueryHistoryMax'] = 100;
-
-/**
- * Whether or not to query the user before sending the error report to
- * the phpMyAdmin team when a JavaScript error occurs
- *
- * Available options
- * ('ask' | 'always' | 'never')
- * default = 'ask'
- */
-//$cfg['SendErrorReports'] = 'always';
-
-/**
- * 'URLQueryEncryption' defines whether phpMyAdmin will encrypt sensitive data from the URL query string.
- * 'URLQueryEncryptionSecretKey' is a 32 bytes long secret key used to encrypt/decrypt the URL query string.
- */
-//$cfg['URLQueryEncryption'] = true;
-//$cfg['URLQueryEncryptionSecretKey'] = '';
-
-/**
- * You can find more configuration options in the documentation
- * in the doc/ folder or at <https://docs.phpmyadmin.net/>.
- */
+?>
